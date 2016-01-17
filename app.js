da57b8a2a5284e36b9ea7767f015109eaefec9b1
Sam Michaels <swmichaels@gmail.com>
2016-01-17 15:20:42 -0600

    working ajax

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
 (function() {
 
   return {
+
     events: {
-      'app.activated':'doSomething'
+      'app.activated': 'init',
+      'app.willDestroy': function() { console.log('app is about ' +
+                                                  'to be destroyed'); }
     },
 
-    doSomething: function() {
+    requests: {
+      getTicketSlaData: function() {
+        var curTicket = this.ticket();
+        return {
+          type: 'GET',
+          url: '/api/v2/tickets/' + curTicket.id() +
+            '?include=slas,metric_events',
+          dataType: 'json'
+        };
+      }
+    },
+
+    init: function(e) {
+      if (e.firstLoad) { this.switchTo('testing'); console.log('first load'); }
+      var slas;
+      var metric_events;
+      this.ajax('getTicketSlaData')
+        .done(function(data) {
+          console.log(data);
+          // only available to professional + plans
+          slas = data.ticket.slas;
+          metric_events = data.ticket.metric_events;
+          this.switchTo('testing', { slas: slas, metric_events: metric_events });
+     
+          console.log(slas);
+          console.log(metric_events);
+
+       });
+      
     }
   };
 
