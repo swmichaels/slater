b130f7571ef5d81e0121a282a38a82c6a3b9db7c
Alexander Culligan <aculligan@icloud.com>
2016-06-11 02:23:53 -0500

    Future dates bug
    Fixed bug causing future dates not to show at times.

--- app.js
+++ app.js
@@ -13,12 +13,13 @@
       'click #first_reply_time_toggle': function(event) {
         this.$('#first_reply_time').toggle();
         var currentTime = Date.now();
-        var currentTimeFormat = moment(currentTime).format('YYYY-MM-DD [at] hh:mm:ss a z');
+        var currentTimeFormat = moment(currentTime).format();
         var userTime = this.$('#first_reply_time #timestamp_breach');
         var userTimeValue = userTime.last().text();
+        var userTimeValueMoment = moment(userTimeValue, 'YYYY-MM-DD [at] hh:mm:ss a z').format();
         var badge = this.$('#first_reply_time #breach');
         var history = this.$('p#first_reply_time.history_complete');
-        if (userTimeValue > currentTimeFormat) {
+        if (userTimeValueMoment >= currentTimeFormat) {
           userTime.last().toggle();
           badge.last().toggle();
           history.css("marginBottom", "-18px");
@@ -27,12 +28,13 @@
       'click #next_reply_time_toggle': function(event) {
         this.$('#next_reply_time').toggle();
         var currentTime = Date.now();
-        var currentTimeFormat = moment(currentTime).format('YYYY-MM-DD [at] hh:mm:ss a z');
+        var currentTimeFormat = moment(currentTime).format();
         var userTime = this.$('#next_reply_time #timestamp_breach');
         var userTimeValue = userTime.last().text();
+        var userTimeValueMoment = moment(userTimeValue, 'YYYY-MM-DD [at] hh:mm:ss a z').format();
         var badge = this.$('#next_reply_time #breach');
         var history = this.$('p#next_reply_time.history_complete');
-        if (userTimeValue > currentTimeFormat) {
+        if (userTimeValueMoment >= currentTimeFormat) {
           userTime.last().toggle();
           badge.last().toggle();
           history.css("marginBottom", "-18px");
@@ -41,12 +43,13 @@
       'click #periodic_update_time_toggle': function(event) {
         this.$('#periodic_update_time').toggle();
         var currentTime = Date.now();
-        var currentTimeFormat = moment(currentTime).format('YYYY-MM-DD [at] hh:mm:ss a z');
+        var currentTimeFormat = moment(currentTime).format();
         var userTime = this.$('#periodic_update_time #timestamp_breach');
         var userTimeValue = userTime.last().text();
+        var userTimeValueMoment = moment(userTimeValue, 'YYYY-MM-DD [at] hh:mm:ss a z').format();
         var badge = this.$('#periodic_update_time #breach');
         var history = this.$('p#periodic_update_time.history_complete');
-        if (userTimeValue > currentTimeFormat) {
+        if (userTimeValueMoment >= currentTimeFormat) {
           userTime.last().toggle();
           badge.last().toggle();
           history.css("marginBottom", "-18px");
@@ -55,12 +58,13 @@
       'click #requester_wait_time_toggle': function(event) {
         this.$('#requester_wait_time').toggle();
         var currentTime = Date.now();
-        var currentTimeFormat = moment(currentTime).format('YYYY-MM-DD [at] hh:mm:ss a z');
+        var currentTimeFormat = moment(currentTime).format();
         var userTime = this.$('#requester_wait_time #timestamp_breach');
         var userTimeValue = userTime.last().text();
+        var userTimeValueMoment = moment(userTimeValue, 'YYYY-MM-DD [at] hh:mm:ss a z').format();
         var badge = this.$('#requester_wait_time #breach');
         var history = this.$('p#requester_wait_time.history_complete');
-        if (userTimeValue > currentTimeFormat) {
+        if (userTimeValueMoment >= currentTimeFormat) {
           userTime.last().toggle();
           badge.last().toggle();
           history.css("marginBottom", "-18px");
@@ -69,12 +73,13 @@
       'click #agent_work_time_toggle': function(event) {
         this.$('#agent_work_time').toggle();
         var currentTime = Date.now();
-        var currentTimeFormat = moment(currentTime).format('YYYY-MM-DD [at] hh:mm:ss a z');
+        var currentTimeFormat = moment(currentTime).format();
         var userTime = this.$('#agent_work_time #timestamp_breach');
         var userTimeValue = userTime.last().text();
+        var userTimeValueMoment = moment(userTimeValue, 'YYYY-MM-DD [at] hh:mm:ss a z').format();
         var badge = this.$('#agent_work_time #breach');
         var history = this.$('p#agent_work_time.history_complete');
-        if (userTimeValue > currentTimeFormat) {
+        if (userTimeValueMoment >= currentTimeFormat) {
           userTime.last().toggle();
           badge.last().toggle();
           history.css("marginBottom", "-18px");
